aeb123c6a535533d4588174bf91dcff43f84d520
Sachin Ahuja <sachinahj@gmail.com>
2014-10-16 19:50:56 -0500

    added crowd-surf view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/crowd-surf', function(req, res) {
+  res.render('crowd-surf');
 });
 
 module.exports = router;
